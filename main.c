9c3428bcf8207a1474445a048cc1c7b80f3c5b20
Charith Edirimanne <charithediriphysical2004@gmail.com>
2026-09-18 20:11:57 +0530

    t push Revert "update main c file"
    This reverts commit af986511a93bbbdbb62bcecf6cf2b3fcfe7b492c.

--- main.c
+++ main.c
@@ -12,38 +12,6 @@ int main()
     printf("     SMART HOSPITAL SYSTEM\n");
     printf("====================================\n");
 
-    const char *specialtyName[] = {
-    "",
-    "General Practice (OPD)",
-    "Paediatrics",
-    "Cardiology",
-    "Neurology"
-};
-
-const float specialtyFee[] = {
-    0,
-    1500.00,
-    2500.00,
-    4500.00,
-    5000.00
-};
-
-const int consultationTime[] = {
-    0,
-    15,
-    20,
-    30,
-    30
-};
-
-const int dailyPatientCap[] = {
-    0,
-    30,
-    20,
-    12,
-    10
-};
-
     return 0;
 }
 
